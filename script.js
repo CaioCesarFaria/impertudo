4470fd3fb2b4e04e6485ba76551fdb51f6ed0ca9
CaioCesarFaria <321caioster@gmail.com>
2024-09-19 17:09:23 -0300

    Mais ajustes

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ saibaMaisLink.addEventListener('click', () => {
         content.classList.toggle('active');
     });
 });
+
+// Carrossel de Thumbnails
+var swiperThumbs = new Swiper('.mySwiper', {
+    spaceBetween: 10,
+    slidesPerView: 5,  // Define quantas miniaturas aparecem por vez
+    freeMode: true,
+    watchSlidesProgress: true,  // Garante que o progresso dos slides seja observado
+});
+
+// Carrossel Principal
+var swiperMain = new Swiper('.mySwiper2', {
+    spaceBetween: 10,
+    loop: true,
+    effect: "slide",
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    thumbs: {
+        swiper: swiperThumbs,  // Ligando ao carrossel de miniaturas
+    },
+});
